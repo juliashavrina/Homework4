5a64a63a8c4fc0e268af2eadb9c615017a5dc1d0
«juliashavrina» <tifanys@yandex.ru>
2022-07-07 21:34:16 +0300

    Homework4

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿/*int SumNum(int a)
+{
+    int sum = 0; 
+    int count = 0;
+     while (a > 0 )
+        {
+            int num = a % 10;
+            sum = sum + num;
+            a = a / 10;
+            count = count + 1;
+         }
+    return sum;   
+}
+Console.WriteLine ("Input a number ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine (SumNum(n));
+*/
+
+/*void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next();
+        index++;
+    }
+}
+void PrintArray (int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine (col [position]);
+        position++;
+
+    }
+}
+    
+    Console.WriteLine ("Input a number ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int [] array = new int [n];
+    FillArray (array);
+    PrintArray (array);
+    */
+
+
+
+
+
+
+
+
+
+
+
